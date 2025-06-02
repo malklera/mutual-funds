5a50f7c97e126d26f0f7be8bd5a88089b2da7883
malklera <leovanlacke@gmail.com>
2025-06-02 19:57:38 -0300

    create the main file, choose the argument i will use and manage the different options

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	saveValues()
+	switch {
+	case len(os.Args) == 1:
+		// Show the menu here
+	case len(os.Args) == 2:
+		if os.Args[1] == "-u" {
+			// Run saveValues()
+			fmt.Println("The argument is:", os.Args[1])
+		} else {
+			fmt.Println("Wrong argument:", os.Args[1])
+		}
+	default:
+		fmt.Println("Too many arguments.")
+	}
 }
