de1d4372c99bc7f0a44c966601c0ae3f31a1929a
malklera <leovanlacke@gmail.com>
2025-06-02 19:24:58 -0300

    i have a base funds.json(the backup is tmp/url-funds.json), i can read from the funds, check the info and add a new value

---
+++ saveValues.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/chromedp/chromedp"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Fund struct {
+	Name  string       `json:"name"`
+	Url   string       `json:"url"`
+	Risk  string       `json:"risk"`
+	Value []ValueEntry `json:"value"`
+}
+
+type ValueEntry struct {
+	Date  string  `json:"date"`
+	Price float64 `json:"price"`
+}
+
+const (
+	name  = `[data-testid="titleDetailDesktop"]`
+	risk  = `[data-testid="fundRiskDetailName"]`
+	value = `[data-testid="currentShareValueType"]`
+	file  = "funds.json"
+)
+
+func saveValues() {
+	funds := updateValues()
+	updatedFunds, err := json.MarshalIndent(funds, "", "\t")
+	if err != nil {
+		log.Fatalf("Error marshaling json from funds: %v", err)
+	}
+
+	err = os.WriteFile(file, updatedFunds, 0666)
+	if err != nil {
+		log.Fatalf("Error writing file %s: %v", file, err)
+	}
+}
+
+func updateValues() []Fund {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		log.Fatalf("Error reading file %s: %v", file, err)
+	}
+
+	var funds []Fund
+	err = json.Unmarshal(data, &funds)
+	if err != nil {
+		log.Fatalf("Error unmarshaling file %s: %v", file, err)
+	}
+
+	var newFunds []Fund
+
+	for _, fund := range funds {
+		newFunds = append(newFunds, getInfo(fund))
+	}
+
+	return newFunds
+}
+
+func getInfo(fund Fund) Fund {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	var resName string
+	var resRisk string
+	var resValue string
+	date := time.Now().Format(time.DateOnly)
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(fund.Url),
+		chromedp.Text(name, &resName, chromedp.NodeVisible),
+		chromedp.Text(risk, &resRisk, chromedp.NodeVisible),
+		chromedp.Text(value, &resValue, chromedp.NodeVisible),
+	)
+
+	if err != nil {
+		log.Printf("Error with url:\n%s\n", fund.Url)
+		log.Fatalln(err)
+	}
+
+	if resName != fund.Name {
+		log.Printf("Error with url:\n%s\n", fund.Url)
+		log.Println("Name of fund has changed.")
+		log.Fatalln(err)
+	}
+
+	if resRisk != fund.Risk {
+		log.Printf("Risk of fund change from '%s' to '%s'", fund.Risk, resRisk)
+		fund.Risk = resRisk
+	}
+
+	resValue = strings.TrimPrefix(resValue, "$ ")
+	resValue = strings.ReplaceAll(resValue, ".", "")
+	resValue = strings.ReplaceAll(resValue, ",", ".")
+	resValueFloat, err := strconv.ParseFloat(resValue, 64)
+	if err != nil {
+		log.Fatalf("Error trying to convert %s to int: %v", strings.TrimPrefix(resValue, "$ "), err)
+	}
+
+	fund.Value = append(fund.Value, ValueEntry{Date: date, Price: resValueFloat})
+
+	return fund
+}
